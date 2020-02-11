2ca4db395e5cf7307250c6cf8bad2035a5400ef8
grca3 <grca42222@gmail.com>
2020-02-11 16:47:03 +0900

    JQueryにて改
    スライドショーはslickプラグイン

---
+++ public/main.js
@@ -0,0 +1,14 @@
+// $(document).ready(function(){
+//   $('.app-list').click(function() {
+//     var $apps = $(this).find('.apps');
+//     if ($apps.hasClass('open')) {
+//       $apps.removeClass('open');
+//       $apps.slideUp();
+//       $(this).find('span').text('+');
+//     } else {
+//       $apps.addClass('open');
+//       $apps.slideDown();
+//       $(this).find('span').text('-');
+//     }
+//   });
+  
